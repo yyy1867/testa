cc75a103c87b8c6fb9978a114f8ea1c25fe82481
29582146@qq.com <aaa258123>
2018-02-26 09:06:00 +0800

    更改启动类路径,方便发布的时候指定配置文件

--- testkotlin/src/main/kotlin/ml/guxing/test/core/home/action/RestHomeController.kt
+++ testkotlin/src/main/kotlin/ml/guxing/test/core/home/action/RestHomeController.kt
@@ -4,9 +4,23 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.cloud.netflix.feign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+
+@FeignClient("test-groovy")
+interface BaseActionController {
+    @GetMapping("")
+    fun index(): String
+
+    @GetMapping("/getMenus")
+    fun getRootmenus(): String
+
+    @GetMapping("/listServices")
+    fun getListServices(): String
+}
 
 @RestController
 class RestHomeController {
@@ -17,6 +31,8 @@ class RestHomeController {
     var client: DiscoveryClient? = null
     @Autowired
     var restTemplate: RestTemplate? = null
+    @Autowired
+    var baseActionController: BaseActionController? = null
 
     @RequestMapping("")
     fun home(): String {
@@ -27,8 +43,9 @@ class RestHomeController {
     fun helloword(): String {
         var instance = client?.localServiceInstance
         logger.info("/hello,host:${instance?.host},serviceId:${instance?.serviceId}")
-        println("====test-kotlin")
-        val body = restTemplate?.getForEntity("http://test-kotlin", String::class.java)?.body
+//        println("====test-kotlin")
+//        val body = restTemplate?.getForEntity("http://test-kotlin", String::class.java)?.body
+        var listServices = baseActionController?.getListServices()
         return "Hello Word!!!"
     }
 
--- testkotlin/src/test/kotlin/ml/guxig/test/core/TestKotlinMain.kt
+++ testkotlin/src/test/kotlin/ml/guxig/test/core/TestKotlinMain.kt
@@ -1,13 +1,15 @@
-package ml.guxing.test.core
+package ml.guxig.test.core
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.feign.EnableFeignClients
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
 
 @EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 open class MyConfig {
 
